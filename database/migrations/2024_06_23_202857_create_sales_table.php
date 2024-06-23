075500adab47b7eeac9c50eda96dc528a2e055a7
Eduardo Henrique <eduardo.hor@outlook.com>
2024-06-23 17:50:09 -0300

    creating migration, model and controller for customer, product, sales and installments

---
+++ database/migrations/2024_06_23_202857_create_sales_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                    ->constrained()
+                    ->cascadeOnDelete()
+                    ->cascadeOnUpdate();
+            $table->foreignId('customer_id')
+                    ->nullable()
+                    ->cascadeOnDelete()
+                    ->cascadeOnUpdate();
+            $table->decimal('total_amount', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
---
+++ database/migrations/2024_06_23_204129_create_installments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('installments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sale_id')
+                    ->constrained()
+                    ->cascadeOnUpdate()
+                    ->cascadeOnDelete();
+            $table->integer('installment_number');
+            $table->date('due_date');
+            $table->decimal('amount', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('installments');
+    }
+};
